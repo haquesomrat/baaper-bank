deca2be68d84ba755137daccfbbe606f8c0457db
haquesomrat <ihsomrat.official@gmail.com>
2021-08-18 11:52:28 +0600

    handler added

--- js/bank.js
+++ js/bank.js
@@ -1,3 +1,12 @@
-document.getElementById("login-aubmit").addEventListener("click", function(){
-    console.log("button clicked");
+document.getElementById('login-submit').addEventListener('click', function(){
+    //get user mail
+    const emailField =    document.getElementById('user-email');
+    const userMail = emailField.value;
+    console.log(userMail);
+    //get user password
+    document.getElementByIdd('login-submit').addEventListener('click', function(){
+        const userField = document.getElementById('user-password');
+        const userPassword = userField.value;
+        console.log(userPassword); 
+    })
 })
