ca984b0b2141db52f11b4c02b5e95da9609cc81d
haquesomrat <ihsomrat.official@gmail.com>
2021-08-18 12:11:13 +0600

    linked with banking.html

--- js/bank.js
+++ js/bank.js
@@ -2,11 +2,16 @@ document.getElementById('login-submit').addEventListener('click', function(){
     //get user mail
     const emailField =    document.getElementById('user-email');
     const userMail = emailField.value;
-    console.log(userMail);
+    // console.log(userMail);
     //get user password
     document.getElementByIdd('login-submit').addEventListener('click', function(){
         const userField = document.getElementById('user-password');
         const userPassword = userField.value;
-        console.log(userPassword); 
+        // console.log(userPassword); 
+        
+        //check email and password
+        if (userMail  == 'ihsomrat.official@gmail.com' &&  userPassword == 82862341){
+            window.location.href = 'banking.html';
+        }
     })
 })
