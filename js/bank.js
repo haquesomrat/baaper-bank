2c2d11d1d38d0fcc341959770c107301afad5be2
Injamul Haque <ihsomrat.official@gmail.com>
2021-08-19 20:12:17 +0600

    yes um doin again

--- js/bank.js
+++ js/bank.js
@@ -1,17 +1,15 @@
-document.getElementById('login-submit').addEventListener('click', function(){
+document.getElementById('login-submit').addEventListener('click', function () {
     //get user mail
-    const emailField =    document.getElementById('user-email');
+    const emailField = document.getElementById('user-email');
     const userMail = emailField.value;
     // console.log(userMail);
     //get user password
-    document.getElementByIdd('login-submit').addEventListener('click', function(){
-        const userField = document.getElementById('user-password');
-        const userPassword = userField.value;
-        // console.log(userPassword); 
-        
-        //check email and password
-        if (userMail  == 'ihsomrat.official@gmail.com' &&  userPassword == 82862341){
-            // window.location.href = 'banking.html';
-        }
-    })
-})
+    const userField = document.getElementById('user-password');
+    const userPassword = userField.value;
+    // console.log(userPassword); 
+
+    //check email and password
+    if (userMail == 'ihsomrat.official@gmail.com' && userPassword == '82862341') {
+        // window.location.href = 'banking.html';
+    }
+});
